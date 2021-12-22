159199e3b23ca0bfac72f804fba874975e8aeb6d
sadhuchetan98 <sadhuchetan98@gmail.com>
2021-12-22 17:23:43 +0530

    Add project files.

---
+++ DemoASPapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoASPapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
